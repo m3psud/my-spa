e3f02f3c8756adcac1446f281d5f0e740c4e3ebf
mildred morales <mildredmoralesalmeida1503@gmail.com>
2023-01-15 00:59:12 -0300

    feat:Iniciando SPA con Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { WikipediaService } from './wikipedia.service';
+
+
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'wsearch';
+ 
+  pages= [] ;
+
+  constructor( private wikipedia: WikipediaService) {}
+  
+  onTerm(term: string) {
+  
+   this.wikipedia.search(term).subscribe((response:any =[]) =>{
+     this.pages = response.query.search;
+     console.log(this.pages)
+   });
+  
+  }
 }
